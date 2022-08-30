4ff916c6e3e9a4ce711de74bfa15328617db0517
Paula da Silva <carolpaulag@gmail.com>
2022-08-30 13:59:38 +0000

    Create guess to track the correctly word the user imput

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import random
+import string
 from words import words 
 
 def get_value_word(words):
-    """ 
+    """
     Get a valid word from the list
     """
     word = random.choice(words)
@@ -10,3 +11,15 @@ def get_value_word(words):
         word = random.choice(words)
 
     return word
+
+def guess():
+    """
+    Track the correctly word the user guess
+    """
+    word = get_value_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+user_input = input('Type somenthing: ')
+print(user_input)
