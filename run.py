a9bbb0bd79f96decb198cd842665ba95615d556c
Paula da Silva <carolpaulag@gmail.com>
2022-08-30 13:35:41 +0000

    Create get_values_word function to get a valid word from the list

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 import random
 from words import words 
 
-print(words)
+def get_value_word(words):
+    """ 
+    Get a valid word from the list
+    """
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
