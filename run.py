6a12953c27f679d98f9729439bfbb02422315c0b
Paula da Silva <carolpaulag@gmail.com>
2022-08-31 10:29:38 +0000

    Create a list with all letter the user have not guess

--- run.py
+++ run.py
@@ -22,17 +22,25 @@ def guess():
     used_letters = set()
 
     # Getting user input
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0:
+        # letters used
 
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again.')
+        print('You have used these letters: ', ' '.join(used_letters))
 
-    else:
-        print('Invalid character. Please try again.')
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid character. Please try again.')
         
 
 guess()
