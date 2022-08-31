16c64ccc09ae85a9875bab7ffcb978265919ea53
Paula da Silva <carolpaulag@gmail.com>
2022-08-31 09:50:26 +0000

    Add function to get user input letter

--- run.py
+++ run.py
@@ -14,12 +14,25 @@ def get_value_word(words):
 
 def guess():
     """
-    Track the correctly word the user guess
+    Track the correctly word the user input
     """
     word = get_value_word(words)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-user_input = input('Type somenthing: ')
-print(user_input)
+    # Getting user input
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('You have already used that character. Please try again.')
+
+    else:
+        print('Invalid character. Please try again.')
+        
+
+guess()
