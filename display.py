0a21c05b910d55b4fd4ee89adc5c104f12e7769e
Paula da Silva <carolpaulag@gmail.com>
2022-09-04 15:14:02 +0000

    update lives and display.py

--- display.py
+++ display.py
@@ -1,44 +1,57 @@
 lives_display = {
-    # display: head, torso, 2 arms, 2 legs
-    0: '''
+  # display: empty
+    0:                                                       
+    '''
         +-----+
-        |     |
-        o     |
-       /|\    |
-       / \    |
+              |
+              |
+              |
+              |
               |
               |
               |
     ===========
     ''',
-    # display: head, torso, 2 arms, 1 leg
-    1: 
+    # display: empty
+    1:                                                       
+    '''
+        +-----+
+        |     |
+              |
+              |
+              |
+              |
+              |
+              |
+    ===========
+    ''',
+     # display: head
+    2: '''
+        +-----+
+        |     |
+        o     |
+              |
+              |
+              |
+              |
+              |
+    ===========
+    ''',
+      # display: head, torso
+    3: 
     '''
         +-----+
         |     |
         o     |
-       /|\    |
-       /      |
-              |
-              |
-              |
-    ===========
-    ''',
-    # display: head, torso, 2 arms
-    2:
-    '''
-        +-----+
         |     |
-        o     |
-       /|\    |
               |
               |
               |
               |
     ===========
     ''',
-    # display: head, torso, 1 arm
-    3:
+       # display: head, torso, 1 arm
+    4:
     '''
         +-----+
         |     |
@@ -50,46 +63,43 @@ lives_display = {
               |
     ===========
     ''',
-     # display: head, torso
-    4: 
+    # display: head, torso, 2 arms
+    5:
     '''
         +-----+
         |     |
         o     |
-        |     |
+       /|\    |
               |
               |
               |
               |
     ===========
     ''',
-  
-     # display: head
-    5: '''
+    # display: head, torso, 2 arms, 1 leg
+    5: 
+    '''
         +-----+
         |     |
         o     |
-              |
-              |
+       /|\    |
+       /      |
               |
               |
               |
     ===========
     ''',
+    # display: head, torso, 2 arms, 2 legs
+    6: '''
+        +-----+
+        |     |
+        o     |
+       /|\    |
+       / \    |
+              |
+              |
+              |
+    ===========
+    '''   
  
-    # display: empty
-    6:                                                       
-    '''
-        +-----+
-        |     |
-              |
-              |
-              |
-              |
-              |
-              |
-    ===========
-    '''
 } 
-
-print(lives_display)
